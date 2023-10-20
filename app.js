5de46e93be9157b8258a94c0e9c49116695455bd
{n-rick2972} <{skyhigh.kbkr2972@gmail.com}>
2023-10-20 19:59:13 +0900

    ✨ 各パラメーターを指定できる機能を追加

--- app.js
+++ app.js
@@ -3,10 +3,59 @@ const qrText = document.getElementById("qrText");
 const qrImage = document.getElementById("qrImage");
 const btn = document.querySelector("button");
 
+// サイズ指定(同値にする)
+const qrHeight = document.getElementById("qrHeight");
+const qrWidth = document.getElementById("qrWidth");
+
+qrHeight.addEventListener("keydown", () => {
+  qrWidth.value = qrHeight.value;
+});
+
+// 選択されているカラーコードを取得
+const colorCode = () => {
+  const colorBox = document.getElementById("color");
+  switch (colorBox.selectedIndex) {
+    case 1:
+      return "0000ff";
+      break;
+    case 2:
+      return "000080";
+      break;
+    case 3:
+      return "008000";
+      break;
+    case 4:
+      return "ffff00";
+      break;
+    case 5:
+      return "ff0000";
+      break;
+    case 6:
+      return "800080";
+      break;
+    default:
+      return "333";
+  }
+};
+
+// 選択されているカラーコードを取得
+const selectFormat = () => {
+  const formatBox = document.getElementById("format");
+  return formatBox.selectedIndex === 0 ? "png" : "svg";
+};
+
+// QRコード生成
 GenerateQR = () => {
   btn.addEventListener("click", () => {
     if (qrText.value.length > 0) {
-      qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+      qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=${
+        qrHeight.value
+      }x${qrWidth.value}&data=${
+        qrText.value
+      }&color=${colorCode()}&format=${selectFormat()}`;
+
+      console.log(qrImage.src);
+
       imgBox.classList.add("is-show");
     } else {
       qrText.classList.add("error");
@@ -16,5 +65,11 @@ GenerateQR = () => {
     }
   });
 };
-
 GenerateQR();
+
+// リセットボタン
+const resetBtn = document.getElementById("resetBtn");
+
+resetBtn.addEventListener("click", () => {
+  location.reload();
+});
